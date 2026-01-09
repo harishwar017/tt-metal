964b303871f1263ac3b75403e5fc1c1b4ad97e85
Nikola Cvetkovic <ncvetkovic@tenstorrent.com>
2026-01-09 22:14:54 +0000

    #0: Add a more visible and verbose todo

--- tt_metal/include/compute_kernel_api/pack_untilize.h
+++ tt_metal/include/compute_kernel_api/pack_untilize.h
@@ -63,7 +63,8 @@ ALWI void pack_untilize_dest_init(uint32_t ocb, uint32_t face_r_dim = 16, uint32
     // Needed for setting swizzle_32b:
     MATH((llk_math_reconfig_remap(true)));
 #endif
-    // A workaround for tt-metal#17132. Should be addressed more systematically.
+    // TODO NC: A workaround for tt-metal#17132. Should be addressed more systematically in tt-llk#989
+
     PACK(
         (llk_pack_untilize_hw_configure_disaggregated<DST_ACCUM_MODE, false /*untilize*/>(ocb, face_r_dim, num_faces)));
     PACK((llk_pack_untilize_init<block_ct_dim, full_ct_dim, false, narrow_row, row_num_datums>(

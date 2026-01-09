d4ba78e5ea4aef37becde273dc7d07a14b71697a
Nikola Cvetkovic <ncvetkovic@tenstorrent.com>
2026-01-09 22:02:15 +0000

    #0: Remove llk_pack_hw_configure_disaggregated

--- tt_metal/include/compute_kernel_api/bcast.h
+++ tt_metal/include/compute_kernel_api/bcast.h
@@ -50,7 +50,7 @@ ALWI void unary_bcast_init(uint32_t icb, uint32_t ocb) {
     MATH((llk_math_hw_configure<DST_ACCUM_MODE>(icb, icb)));
 #endif
 
-    PACK((llk_pack_hw_configure_disaggregated<DST_ACCUM_MODE, false>(ocb)));
+    PACK((llk_pack_hw_configure<DST_ACCUM_MODE>(ocb)));
     PACK((llk_pack_init<false>(ocb)));
     PACK((llk_pack_dest_init<DST_ACCUM_MODE, false>()));
 }
@@ -244,7 +244,7 @@ void init_bcast(uint32_t icb0, uint32_t icb1, uint32_t ocb) {
     UNPACK((llk_unpack_hw_configure<DST_ACCUM_MODE>(icb0, icb1)));
     UNPACK((llk_unpack_AB_init<tBcastDim>(icb0, icb1)));
 
-    PACK((llk_pack_hw_configure_disaggregated<DST_ACCUM_MODE, false>(ocb)));
+    PACK((llk_pack_hw_configure<DST_ACCUM_MODE>(ocb)));
     PACK((llk_pack_init(ocb)));
     PACK((llk_pack_dest_init<DST_ACCUM_MODE, false>()));
 
--- tt_metal/include/compute_kernel_api/pack_untilize.h
+++ tt_metal/include/compute_kernel_api/pack_untilize.h
@@ -36,7 +36,7 @@ namespace ckernel {
  * NOTE: This function allows the user to specify `face_r_dim` and `num_faces` through function parameters. Setting these
  * parameters results in an expensive MMIO write and cannot be avoided currently.
  * This should be addressed more systematically within the issue tt-metal#22820, since these two values can be inferred
- * from the circular buffer description, the same way as it is done in `llk_pack_hw_configure_disaggregated`. This
+ * from the circular buffer description, the same way as it is done in `llk_pack_hw_configure`. This
  * would remove the need for `llk_pack_untilize_hw_configure_disaggregated` altogether and we would pay the price
  * of the MMIO write only once, in `compute_kernel_hw_startup`.
  *
@@ -89,7 +89,7 @@ ALWI void pack_untilize_dest_init(uint32_t ocb, uint32_t face_r_dim = 16, uint32
  * NOTE: This function uses default `face_r_dim` and `num_faces` values (16 and 4, respectively). Setting these
  * parameters results in an expensive MMIO write and cannot be avoided currently.
  * This should be addressed more systematically within the issue tt-metal#22820, since these two values can be inferred
- * from the circular buffer description, the same way as it is done in `llk_pack_hw_configure_disaggregated`. This
+ * from the circular buffer description, the same way as it is done in `llk_pack_hw_configure`. This
  * would remove the need for `llk_pack_untilize_hw_configure_disaggregated` altogether and we would pay the price
  * of the MMIO write only once, in `compute_kernel_hw_startup`.
  *

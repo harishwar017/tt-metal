f5ce059f166d764d9c24c7b40913fee486d2e2fc
grechsteinerTT <grechsteiner@tenstorrent.com>
2026-01-09 17:22:17 +0000

    Everything compiling, time to start gutting

--- ttnn/cpp/ttnn/operations/experimental/ccl/ccl_experimental_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/ccl_experimental_nanobind.cpp
@@ -27,6 +27,7 @@
 #include "ttnn/operations/experimental/ccl/slice_reshard_async/slice_reshard_async_nanobind.hpp"
 #include "ttnn/operations/experimental/ccl/strided_all_gather_async/strided_all_gather_async_nanobind.hpp"
 #include "ttnn/operations/experimental/ccl/deepseek_minimal_broadcast/deepseek_minimal_broadcast_nanobind.hpp"
+#include "ttnn/operations/experimental/ccl/deepseek_reduce_scatter/deepseek_reduce_scatter_nanobind.hpp"
 
 namespace ttnn::operations::experimental::ccl {
 
@@ -52,6 +53,7 @@ void py_module(nb::module_& mod) {
     ccl::bind_neighbor_pad_async(mod);
     ccl::bind_slice_reshard_async(mod);
     ccl::bind_deepseek_minimal_broadcast(mod);
+    ccl::bind_deepseek_reduce_scatter(mod);
 }
 
 }  // namespace ttnn::operations::experimental::ccl

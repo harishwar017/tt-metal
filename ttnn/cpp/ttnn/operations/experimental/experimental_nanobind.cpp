1c282613ef54820ad5e76ca03e9e6feef35fd1e9
Ryan Miller <rmiller@tenstorrent.com>
2026-01-16 19:55:06 +0000

    More parallel infra, unit test

--- ttnn/cpp/ttnn/operations/experimental/experimental_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/experimental/experimental_nanobind.cpp
@@ -54,6 +54,7 @@
 #include "ttnn/operations/experimental/test/hang_device/hang_device_operation_nanobind.hpp"
 #include "ttnn/operations/experimental/minimal_matmul/minimal_matmul_nanobind.hpp"
 #include "ttnn/operations/experimental/isin/isin_nanobind.hpp"
+#include "ttnn/operations/experimental/parallel/parallel_nanobind.hpp"
 
 namespace ttnn::operations::experimental {
 
@@ -127,6 +128,8 @@ void py_module(nb::module_& mod) {
     minimal_matmul::detail::bind_minimal_matmul(mod);
 
     isin::detail::bind_isin_operation(mod);
+
+    parallel::detail::bind_parallel_operation(mod);
 }
 
 }  // namespace ttnn::operations::experimental

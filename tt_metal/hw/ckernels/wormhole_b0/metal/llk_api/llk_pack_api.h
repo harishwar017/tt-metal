dad19a537f74e4893d6cc0ba5b78b8b09a871903
Nikola Cvetkovic <ncvetkovic@tenstorrent.com>
2026-01-16 09:07:12 +0000

    #33830: Remove include_setup_calls flags

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_pack_api.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_pack_api.h
@@ -105,7 +105,7 @@ inline void llk_pack_init(const std::uint32_t pack_output = 16) {
     const bool narrow_tile = get_output_narrow_tile(output_id);
 
     _llk_pack_init_<untilize, zero_output>(
-        pack_dst_format[output_id], pack_src_format[output_id], face_r_dim, num_faces, partial_face, narrow_tile, true);
+        pack_dst_format[output_id], pack_src_format[output_id], face_r_dim, num_faces, partial_face, narrow_tile);
 }
 
 template <bool out_of_order_output, bool untilize>
@@ -169,7 +169,7 @@ inline void llk_pack_untilize_init(
     const std::uint32_t output_id = get_output_id(output);
 
     _llk_pack_untilize_init_<block_ct_dim, full_ct_dim, diagonal, narrow_row, row_num_datums>(
-        pack_dst_format[output_id], face_r_dim, num_faces, true);
+        pack_dst_format[output_id], face_r_dim, num_faces);
 }
 
 template <

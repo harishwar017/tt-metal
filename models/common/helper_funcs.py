931e1aa3099f862c1025e27fb19ca4d18c282eda
harishwar017 <harishwar017@gmail.com>
2026-01-20 10:15:39 +0000

    indus model helper

--- models/common/helper_funcs.py
+++ models/common/helper_funcs.py
@@ -29,15 +29,21 @@ def Linear(
     if bias is not None:
         assert bias.padded_shape[-1] == out_features, "bias does not have the expected shape"
 
-    weight = weight
+    # weight = weight
+    weight = ttnn.to_layout(weight, ttnn.TILE_LAYOUT)
     bias = bias
     weight_T = ttnn.transpose(weight, -2, -1)
 
     def linear_(activation):
         nonlocal bias
         assert activation.padded_shape[-1] == in_features, "activation tensor do not have the expected shape"
-        if bias is not None and bias.get_layout() != ttnn.TILE_LAYOUT:
+        # if bias is not None and bias.get_layout() != ttnn.TILE_LAYOUT:
+        if bias is not None:
             bias = ttnn.to_layout(bias, ttnn.TILE_LAYOUT)
-        return ttnn.linear(activation, weight_T, bias=bias, memory_config=output_mem_config)
+        # return ttnn.linear(activation, weight_T, bias=bias, memory_config=output_mem_config)
+        # Ensure activation is in TILE layout for linear operation
+        activation = ttnn.to_layout(activation, ttnn.TILE_LAYOUT)
+        output = ttnn.linear(activation, weight_T, bias=bias, memory_config=output_mem_config)
+        return output
 
     return linear_

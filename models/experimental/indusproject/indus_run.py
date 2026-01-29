6487274f18ba0d28180c6ff49154c0453324e5e8
harishwar017 <harishwar017@gmail.com>
2026-01-29 11:26:40 +0000

    run file to generate text

--- models/experimental/indusproject/indus_run.py
+++ models/experimental/indusproject/indus_run.py
@@ -46,60 +46,18 @@ def format_template(user_prompt):
 
 user_prompt = """भारत की राजधानी क्या है?"""
 
-# format_template already returns tokenized input_ids
 test_input_ids = format_template(user_prompt)
-print(f"Test input shape: {test_input_ids.shape}")
-# def format_chat_batch(conversations):
-#     """
-#     conversations: List[List[Dict]]
-#     """
-
-#     return tokenizer.apply_chat_template(
-#         conversations,
-#         tokenize=True,
-#         add_generation_prompt=True,
-#         return_tensors="pt",
-#         padding=True,
-#     )
-
-# with open("models/experimental/indusproject/batch32_convs.json", "r", encoding="utf-8") as f:
-#     convs = json.load(f)
-# test_input_ids = format_chat_batch(convs)
-# print("Shape:", test_input_ids.shape)
+print(f"Test input: {user_prompt}")
 
 
-# eos_id = tokenizer.eos_token_id
-
-# stats = tt_model.benchmark_generate(
-#     idx=test_input_ids,
-#     max_new_tokens=32,
-#     eos_id=tokenizer.eos_token_id,
-#     runs=5,
-# )
-
-timing = tt_model.benchmark_generate(idx=test_input_ids, max_new_tokens=32, eos_id=tokenizer.eos_token_id)
-print(f"TT Generate ttft_avg: {timing['ttft_avg']} seconds")
-print(f"TT Generate tps_avg: {timing['tps_avg']} seconds")
-
-print("done")
-# output_ids = tt_model.generate_1(
-#     idx=test_input_ids,
-#     do_sample=False,
-#     # max_new_tokens=10,
-#     temperature=1.0,
-#     eos_id=eos_id
-# )
+output_ids = tt_model.generate_1(idx=test_input_ids, do_sample=False, max_new_tokens=32, eos_id=tokenizer.eos_token_id)
 # print(f"Output shape: {output_ids.shape}")
-# output_ids = ttnn.to_torch(output_ids)
-# text = tokenizer.decode(output_ids[0].tolist(), skip_special_tokens=False)
-# print(f"\nGenerated text:\n{text}")
-# print("\n✓ TT Generate function works!")
-
-# texts = tokenizer.batch_decode(output_ids, skip_special_tokens=False)
-
-# for t in texts:
-#     print(t)
+output_ids = ttnn.to_torch(output_ids)
+text = tokenizer.decode(output_ids[0].tolist(), skip_special_tokens=False)
+print(f"\nGenerated text:\n{text}")
+print("\n✓ TT Generate function works!")
 
 
-# print("TTFT:", stats["ttft_avg"])
-# print("TPS :", stats["tps_avg"])
+# timing = tt_model.benchmark_generate(idx=test_input_ids, max_new_tokens=32, eos_id=tokenizer.eos_token_id)
+# print(f"TT Generate ttft_avg: {timing['ttft_avg']} seconds")
+# print(f"TT Generate tps_avg: {timing['tps_avg']} seconds")
